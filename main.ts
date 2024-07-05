ba4c40e19161ef56f8ba783a3c7401d888028ec4
MisbahNayab12 <160389499+MisbahNayab12@users.noreply.github.com>
2024-07-05 14:51:09 +0500

    Creaated Word Counter

---
+++ main.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+console.log("Welcome to the Word Count Application")
+
+const answer = await inquirer.prompt(
+    [
+        {
+            name: "sentence",
+            type: "string",
+            messsage: "Please Enter your sentence"
+        }
+    ]
+)
+
+let words = answer.sentence.trim().split(" ")
+console.log(words)
+console.log(`Your word count is ${words.length}`)
+
+console.log(" ***** Thank you for using Word Count App ***** ")
